4f0c31a9741efe72f83e30483d12f54a2063aa88
Alexandru Rotari7 <Alexandru Rotari7>
2017-04-13 19:19:28 +0300

    Implemented form validation. Changed dropdown menu to hover menu.

--- src/main/java/com/inthergroup/internship/controllers/HomeController.java
+++ src/main/java/com/inthergroup/internship/controllers/HomeController.java
@@ -99,4 +99,9 @@ public class HomeController {
     public String benefit_type() {
         return "/benefits/benefit-type";
     }
+    
+    @RequestMapping("/validation")
+    public String validation() {
+        return "/validation";
+    }
 }
